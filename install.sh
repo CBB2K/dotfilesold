ca6ba0661e8fd2986ff5684981742add91c82064
CBB2K <christianbbarnhill@gmail.com>
2024-07-10 18:38:26 -0400

    Removed nvim for now and picom config

--- install.sh
+++ install.sh
@@ -4,24 +4,24 @@
 # nvim #
 ########
 
-mkdir -p "$XDG_CONFIG_HOME/nvim"
-mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
+#mkdir -p "$XDG_CONFIG_HOME/nvim"
+#mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
-ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
+#ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
 
-rm -rf "$XDG_CONFIG_HOME/X11"
-ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
+#rm -rf "$XDG_CONFIG_HOME/X11"
+#ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
 # install neovim plugin manager
-[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
-    && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
+#    && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
+#   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
-ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
+#mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
+#ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
 
 # Install (or update) all the plugins
-nvim --noplugin +PlugUpdate +qa
+#nvim --noplugin +PlugUpdate +qa
 
 ######
 # i3 #
@@ -54,6 +54,13 @@ cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
 mkdir -p "$XDG_CONFIG_HOME/dunst"
 ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
+
+#########
+# picom #
+#########
+
+mkdir -p "$XDG_CONFIG_HOME/picom"
+ln -sf "$DOTFILES/picom/picom.conf" "$XDG_CONFIG_HOME/picom/picom.conf"
 
 ########
 # tmux #
